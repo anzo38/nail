f325abf41c8fabb14e61061308164cc89b966297
zoey <anzo.sa8@gmail.com>
2020-11-13 21:42:06 +0900

    no message

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+// $(function() {
+//   $('.slider').slick({
+//     autoplay: true,
+//     autoplaySpeed: 10000,
+//     cssEase: 'ease',
+//     arrows: false,
+//     centerMode: true,
+//     centerPadding: '30px'
+//   )};
+
+1
+$('.slider').slick();
---
+++ main.js
@@ -0,0 +1,11 @@
+$('.slider').slick();
+
+// 2
+// 3
+// 4
+// 5
+// $('.slider').slick({
+//     autoplay:true,
+//     autoplaySpeed:5000,
+//     dots:true,
+// });
