43855394176203263a449d1b89448dc583957cef
zoey <anzo.sa8@gmail.com>
2020-11-17 01:55:13 +0900

    chenge the filles

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
-$('.slider').slick();
 
-// 2
-// 3
-// 4
-// 5
-// $('.slider').slick({
-//     autoplay:true,
-//     autoplaySpeed:5000,
-//     dots:true,
-// });
+  
+jQuery(function() {
+  var pagetop = $('#page_top');   
+  pagetop.hide();
+  $(window).scroll(function () {
+      if ($(this).scrollTop() > 100) {  //100pxスクロールしたら表示
+          pagetop.fadeIn();
+      } else {
+          pagetop.fadeOut();
+      }
+  });
+  pagetop.click(function () {
+      $('body,html').animate({
+          scrollTop: 0
+      }, 500); //0.5秒かけてトップへ移動
+      return false;
+  });
+});
+
+
+
+$('.slider').slick();
